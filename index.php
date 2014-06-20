b38adac4e5f2583fddaf55a6058027d41ed3cdda
Seltaeb87 <bastiaanbitter87@gmail.com>
2014-06-20 21:29:59 +0200

    initial project version

---
+++ index.php
@@ -0,0 +1,103 @@
+<!doctype=html>
+<html lang ="en">
+<head>
+	<meta charset="utf8" />
+	<link rel="stylesheet" typ="text/css" href="/css/style.css">
+</head>
+	<title>Bastiaan Bitter</title>
+<body>
+	<div id="container">
+		<header id="top_header">
+			<h1>Bastiaan Bitter</h1>
+		</header>
+		<nav id="menu">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="portfolio.php">Portfolio</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</nav>
+
+	<div id="section_news_wrapper">
+		<section id="main_section">
+			<article>
+				<header>
+					<hgroup>
+						<h1>Title of article</h1>
+						<h2>Subtitle of article</h2>
+					</hgroup>
+				</header>
+				<p>What a nice article about php, wow!</p>
+				<footer>
+					<p>- Written by Bastiaan Bitter</p>
+				</footer>
+			</article>
+                          ----------------------------------------------------------------------------------------------------------------------
+			<article>
+				<header>
+					<hgroup>
+						<h1>Mijn eerste post</h1>
+						<h2>Welkom!</h2>
+					</hgroup>
+				</header>
+				<p>
+					Welkom op de website van mij, Bastiaan Bitter.
+					Dit is een blog waar ik laat zien wie ik ben, wat ik doe en wat ik heb gedaan. Ook zal ik regelmatig met vragen komen met betrekking tot webdesign en programmeren. 
+					Over bijvoorbeeld PHP, HTML(5), CSS(3), Javascript, Photoshop, Illustrator, Android apps enzovoort.<br /><br />
+				</p>
+				<p>
+					Mijn eerste tip aan jullie is deze: <a href="https://developers.google.com/web/starter-kit/" target="_blank">Web starter kit</a> van Google Developers. 
+					Dit is nog in Beta, maar het ziet er veelbelovend uit. Hierdoor zal webdesign een stuk gemakkelijker worden.<br /><br />
+				</p>
+				<footer>
+					<p>- Written by Bastiaan Bitter</p>
+				</footer>
+			</article>
+		</section>
+
+		<aside id="sidebar">
+			<h4>Sidebar</h4>
+			Content for sidebar!
+		</aside>
+	</div>
+		<footer id="the_footer">
+		</footer>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
